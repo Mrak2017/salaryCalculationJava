b1b0beb3b90fbefb480eda2791fbb4350192e5fc
Mike <redrik09@gmail.com>
2019-04-16 22:49:53 +0700

    spring boot version update to 2.1.4 + test launch embeded tomcat

--- src/main/java/com/github/mrak2017/salarycalculation/Application.java
+++ src/main/java/com/github/mrak2017/salarycalculation/Application.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.event.EventListener;
 
 import java.util.List;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     @Autowired
     PersonController controller;
@@ -24,7 +25,7 @@ public class Application {
     public void test() {
         List<Person> data = controller.getTestData();
         for (Person person : data) {
-            System.out.println(person.toString());
+            System.out.println(String.format("firstName: %s lastName: %s", person.getFirstName(), person.getLastName()));
         }
     }
 }
--- src/main/java/com/github/mrak2017/salarycalculation/controller/PersonRestController.java
+++ src/main/java/com/github/mrak2017/salarycalculation/controller/PersonRestController.java
@@ -3,6 +3,7 @@ package com.github.mrak2017.salarycalculation.controller;
 import com.github.mrak2017.salarycalculation.model.Person;
 import com.github.mrak2017.salarycalculation.repository.PersonRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +15,11 @@ public class PersonRestController {
 
     PersonRestController(PersonRepository repository) {
         this.repository = repository;
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Spring boot is working!";
     }
 
     @GetMapping("/persons")
