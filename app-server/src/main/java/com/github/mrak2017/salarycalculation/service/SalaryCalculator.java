e9d38c09bcf9fb20cebb818d0d67bb29bfabc61d
Mike <redrik09@gmail.com>
2019-05-25 18:25:11 +0700

    junit init

---
+++ app-server/src/main/java/com/github/mrak2017/salarycalculation/service/SalaryCalculator.java
@@ -0,0 +1,4 @@
+package com.github.mrak2017.salarycalculation.service;
+
+public class SalaryCalculator {
+}
---
+++ app-server/src/test/java/com/github/mrak2017/salarycalculation/service/SalaryCalculatorTest.java
@@ -0,0 +1,27 @@
+package com.github.mrak2017.salarycalculation.service;
+
+import com.github.mrak2017.salarycalculation.model.person.GroupType;
+import com.github.mrak2017.salarycalculation.model.person.Person;
+import com.github.mrak2017.salarycalculation.model.person.Person2Group;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class SalaryCalculatorTest {
+
+	@Test
+	public void getCurrentSalaryTest() {
+		/*Person person = new Person();
+
+		Person2Group person2Group1 = new Person2Group();
+		person2Group1.setPerson(person);
+		person2Group1.setPeriodStart(LocalDate.of(2019,1,1));
+		person2Group1.setPeriodEnd(LocalDate.of(2019,2,1));
+		person2Group1.setGroupType(GroupType.Employee);
+
+		Person2Group person2Group2 = new Person2Group();
+		person2Group2.setPerson(person);
+		person2Group2.setPeriodStart(LocalDate.of(2019,2,2));
+		person2Group2.setGroupType(GroupType.Manager);*/
+	}
+}
