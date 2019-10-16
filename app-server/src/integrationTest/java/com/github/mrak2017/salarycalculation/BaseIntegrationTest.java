4e73842b6d9d5f68ee8479a1870229ff08a4b097
Mike <redrik09@gmail.com>
2019-10-16 21:32:47 +0700

    run integration test in intellij idea

--- app-server/src/integrationTest/java/com/github/mrak2017/salarycalculation/BaseIntegrationTest.java
+++ app-server/src/integrationTest/java/com/github/mrak2017/salarycalculation/BaseIntegrationTest.java
@@ -9,6 +9,7 @@ import com.github.mrak2017.salarycalculation.service.PersonController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
@@ -20,8 +21,9 @@ import java.util.UUID;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment  = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("integrationTest")
 public class BaseIntegrationTest {
 
     @Autowired
